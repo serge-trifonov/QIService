73b86ccecca145cc0d34c72a6263c22b683e44cd
MKazakova <45536947+MKazakova@users.noreply.github.com>
2020-06-19 01:00:33 +0100

    admin settings

--- QIService/src/main/java/cnam/project/QIService/config/WebSecurityConfig.java
+++ QIService/src/main/java/cnam/project/QIService/config/WebSecurityConfig.java
@@ -44,7 +44,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				newUser.setGivenName((String) map.get("given_name"));
 				newUser.setFamilyName((String) map.get("family_name"));
 				newUser.setEmail((String) map.get("email"));
-				newUser.setRole(Role.ADMIN);
+				if(newUser.getEmail().equals("hhead173@gmail.com")) {
+					newUser.setRole(Role.ADMIN);
+				}
+				else {
+					newUser.setRole(Role.STUDENT);
+				}
 				
 				return newUser;
 			});
